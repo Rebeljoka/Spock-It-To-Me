1abe63b9f6da034ab79175470e4281a8f330fc29
Rebeljoka <habib.u99@googlemail.com>
2025-08-18 17:08:08 +0100

    Add: img ids to buttons + interactivity in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-// Custom JS goes here
+const userRockBtn = document.getElementById('rock');
+const userRockImg = document.getElementById('UserRockImg');
+const defaultSrc = 'assets/images/stoneBtn.webp';
+const selectedSrc = 'assets/images/stoneBtnClicked.webp';
+
+userRockBtn.addEventListener('click', function() {
+  userRockImg.src = selectedSrc;
+  setTimeout(() => {
+    userRockImg.src = defaultSrc;
+  }, 200); // 200ms delay before switching back
+});
