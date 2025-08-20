4381860415637cbcb69896557625ea9bebdcf096
lracli <220789043+lratcli@users.noreply.github.com>
2025-08-20 12:21:09 +0100

    implement all thee games types - best of 3, 5, endless.

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,6 +129,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let computerWins = 0;
     let drawnGames = 0;
 
+    let gameComplete = false;
+
     /**------------------------------------------------------------------
      * Get html element references
      */
@@ -153,74 +155,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const restartButtonEl = document.getElementById("restartBtn");
     restartButtonEl.addEventListener("click", resetGame);
 
-    //--------------------------------------------------------------------------------------
-    //--------------------------------------------------------------------------------------
-    /** handles user click events on game move choice buttons */
-    function handleUserMoveChoice(e) {
-
-        const playerChoiceButtonId = e.currentTarget.id;
-        const computerChoice = computerChoiceGenerator(currentDifficulty);
-        if (computerChoice == null) {
-            console.error("null returned from computerChoiceGenerator(), exiting player input event handler");
-            return;
-        }
-
-        console.log(`Player selected ${playerChoiceButtonId}\nComputer selected ${computerChoice}`);
-
-        const playerOutcome = processRound(playerChoiceButtonId, computerChoice, currentGameType);
-        console.log(playerOutcome);
-        updateScores(playerOutcome);
-        displayScores();
-
-        // TODO: Reflect outcome of game in the html from here:
-
-        /** DO RIGHT AT THE END in order to PREVENT IT BIASING THE CPU's GO THIS ROUND
-         * Especially a huge problem for MEDIUM difficulty
-         */
-        playerMoveChoices.push(playerChoiceButtonId);
-        movesThisGame++;
-    }
-
-    //--------------------------------------------------------------------------------------
-    //--------------------------------------------------------------------------------------
-
-    /** Processes round logic
-     * Parameters:
-     * - playerChoice - string representing player choice of move
-     * - computerChoice - string representing computer choice of move
-     * - currentGameType -  bestOf3, 5, Endless
-     * Returns: 
-     * - outcome of the game from the player's perspective
+    /** -----------------------------------------
+     * Event handling functions
      */
 
-    function processRound(playerChoice, computerChoice, currentGameType){
-        // Error handling
-        if(playerChoice == false || computerChoice == false){
-            console.log("Invalid variables passes into processRound() function, returning early");
-            return;
-        }
-        console.log(`Entering processRound(), moves this game so far: ${movesThisGame}`)
-
-        switch(currentGameType){
-            case gameType.BestOf3:
-                // Do game
-                break;
-            case gameType.BestOf5:
-                // DO game
-                break;
-            case gameType.Endless:
-                // Do game
-                break;
-            default:
-                console.log("Invalid game type passed into processRound(), no game processing done");
-        }
-
-
-        let playerWinOutcome = checkIfPlayerWins(playerChoice, computerChoice);
-        return playerWinOutcome;
-    }
-
-    function difficultyChange(e){
+        function difficultyChange(e){a
 
         console.log('Difficulty selected:', e.currentTarget.id);
         let newDifficulty = e.currentTarget.id;
@@ -247,6 +186,123 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    //--------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------
+    /** handles user click events on game move choice buttons */
+    function handleUserMoveChoice(e) {
+
+        // If game is over, simply ignore all user move choices
+        if(gameComplete === true){
+            return;
+        }
+
+        const playerChoiceButtonId = e.currentTarget.id;
+        const computerChoice = computerChoiceGenerator(currentDifficulty);
+        if (computerChoice == null) {
+            console.error("null returned from computerChoiceGenerator(), exiting player input event handler");
+            return;
+        }
+
+        console.log(`Player selected ${playerChoiceButtonId}\nComputer selected ${computerChoice}`);
+
+        const playerOutcome = processRound(playerChoiceButtonId, computerChoice, currentGameType);
+        
+        console.log(playerOutcome);
+        updateScores(playerOutcome);
+        displayScores();
+
+        // if moves this game >= moves for game type
+            // disable player buttons until start or restart is clicked
+        if(gameComplete === true){
+            gameCompleteActions();
+        }
+
+        // TODO: Reflect outcome of game in the html from here:
+
+        /** DO RIGHT AT THE END in order to PREVENT IT BIASING THE CPU's GO THIS ROUND
+         * Especially a huge problem for MEDIUM difficulty*/
+        playerMoveChoices.push(playerChoiceButtonId);
+    }
+
+    //--------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------
+
+    /** Processes round logic
+     * Parameters:
+     * - playerChoice - string representing player choice of move
+     * - computerChoice - string representing computer choice of move
+     * - currentGameType -  bestOf3, 5, Endless
+     * Returns: 
+     * - outcome of the game from the player's perspective
+     */
+
+    function processRound(playerChoice, computerChoice, currentGameType){
+        // Error handling
+        if(playerChoice == false || computerChoice == false) return null;
+        if(gameComplete === true) console.log("Game is complete, processRound should not be called until restart");
+
+        let playerWinOutcome = null;
+        movesThisGame++;
+        console.log(`Entering processRound(), num moves this game so far: ${movesThisGame}`);
+       
+        switch(currentGameType){
+            case gameType.BestOf3:
+                console.log("case: best of 3");
+                if(movesThisGame < 3){
+                    playerWinOutcome = checkIfPlayerWins(playerChoice, computerChoice);
+                }
+                else if(movesThisGame === 3){
+                    playerWinOutcome = checkIfPlayerWins(playerChoice, computerChoice);
+                    gameComplete = true;
+                }else{
+                    console.log("case: best of 3 over, game should no longer be reaching here");
+                }
+                break;
+            case gameType.BestOf5:
+                console.log("case: best of 5");
+                if(movesThisGame < 5){
+                    playerWinOutcome = checkIfPlayerWins(playerChoice, computerChoice);
+                }
+                else if(movesThisGame === 5){
+                    playerWinOutcome = checkIfPlayerWins(playerChoice, computerChoice);
+                    gameComplete = true;
+                }else{
+                    console.log("case: best of 5 over, game should no longer be reaching here");
+                }
+                break;
+            case gameType.Endless:
+                console.log("case: endless");
+                playerWinOutcome = checkIfPlayerWins(playerChoice, computerChoice);
+                break;
+            default:
+                console.log("Invalid game type passed into processRound(), no game processing done");
+        }
+        return playerWinOutcome;
+    }
+
+    /** -----------------------------------------
+     *  Generate appropriate game complete message
+     */
+    function gameCompleteActions(){
+        gameComplete = true;
+        console.log("--------------------------------------");
+        console.log("In gameComplete function:");
+        console.log("Game has reached full round limit");
+        console.log("Display results and game end stuff");
+        console.log(`Current game type: ${currentGameType}\nMoves completed: ${movesThisGame}`);
+        console.log(`playerWins = ${playerWins}, computerWins = ${computerWins}, draws = ${drawnGames}.`)
+        if (playerWins === computerWins){
+            console.log("Overall outcome: DRAW");
+        }
+        else if(playerWins > computerWins){
+            console.log("Overall outcome: PLAYER Wins");
+        }
+        else{
+            console.log("Overall outcome: COMPUTER wins");
+        }
+        console.log("Restart or select new game type or difficulty to play again");
+        console.log("--------------------------------------");
+    }
 
     /** CPU choice function: returns one of the five options as a CPU choice 
      * Difficultly parameter options are easy, medium and hard as per the difficultyLevels object
@@ -410,9 +466,9 @@ document.addEventListener("DOMContentLoaded", function () {
      * This function has placeholder functionality until html page is ready
      */
     function displayScores() {
-        console.log(
-            `Player Score is: ${playerWins}\nComputer score is: ${computerWins}\nDraws is: ${drawnGames}`
-        );
+        // console.log(
+        //     `Player Score is: ${playerWins}\nComputer score is: ${computerWins}\nDraws is: ${drawnGames}`
+        // );
         playerScoreEl.innerText = playerWins;
         cpuScoreEl.innerText = computerWins;
         drawScoreEl.innerText = drawnGames;
@@ -421,13 +477,16 @@ document.addEventListener("DOMContentLoaded", function () {
     /** Reset scores / win counts then push to display suing displayScores() function
      * 
     */
-    function resetGame() {
+    function resetGame(e) {
         playerWins = 0;
         computerWins = 0;
         drawnGames = 0;
         playerMoveChoices.length = 0;
         movesThisGame = 0;
+        gameComplete = false;
         displayScores();
+        console.log(`Game reset: wld: ${playerWins} ${computerWins} ${drawnGames} PMC: ${playerMoveChoices.length
+            } MTG: ${movesThisGame} GC: ${gameComplete}`);
     }
 });
 
